01cb7eeb7bfb0b730f2583ec50073261a81d650e
peiyuchen <crazypei23@gmail.com>
2018-02-23 17:51:15 +0800

    Add a function because the onset of each note must also be normalized.

--- normalize_midi_data.m
+++ normalize_midi_data.m
@@ -2,11 +2,23 @@
 % input     : midiData           -> 原始   的midi data
 % output    : normalizedMidiData -> 正規化後的midi data
 function [midiData] = normalize_midi_data (midiData)
-    %                  1    2    4+8    4+16     4    8+16      8      16    
-    noteValue       = [1  0.5  0.375  0.3125  0.25  0.1875  0.125  0.0625];
-    repNoteValue    = repmat(noteValue, length(midiData), 1);
-    tmp             = midiData(:, 2) - floor(midiData(:, 2)) - repNoteValue; 
+    %                  1   2+4   2    4+8    4+16     4    8+16      8      16    
+    noteValue       = [1  0.75 0.5  0.375  0.3125  0.25  0.1875  0.125  0.0625];
+    repNoteValue    = repmat(noteValue, size(midiData, 1), 1);
+
+    tmp             = midiData(:, 2) - floor(midiData(:, 2)) - repNoteValue;
     tmp(tmp > 0)    = -10;
     [~,idx]         = max(tmp, [], 2);
+    
     midiData(:,2) = noteValue(idx)' + floor(midiData(:,2));
+    
+    % onset 也要改變
+    noteValue       = [noteValue 0];
+    repNoteValue    = repmat(noteValue, size(midiData,1), 1);
+
+    tmp             = midiData(:, 1) - floor(midiData(:, 1)) - repNoteValue;
+    tmp(tmp > 0)    = -10;
+    [~,idx]         = max(tmp, [], 2);
+    
+    midiData(:,1) = noteValue(idx)' + floor(midiData(:,1));
 end
